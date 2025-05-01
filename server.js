0af7f8e087b1fe89e34a68af903a094d157e7be1
cobraanurag <upretianurag7@gmail.com>
2025-05-01 07:59:24 +0000

    init

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve index.html
 
